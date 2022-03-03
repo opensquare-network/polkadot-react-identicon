3daecb87d53c6a429333409964d3e43d01706c77
yoshiyuki <brother-zhou@qq.com>
2022-03-03 16:34:12 +0800

    v1.0.6, remove console.log for errors

--- src/lib/components/IdentityIcon.js
+++ src/lib/components/IdentityIcon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Empty, Polkadot } from './icons';
-import { decodeAddress, encodeAddress,  isHex, isU8a, u8aToHex } from "./crypto";
+import { decodeAddress, encodeAddress, isHex, isU8a, u8aToHex } from "./crypto";
 
 const DEFAULT_SIZE = 64;
 const Components = {
@@ -11,10 +11,15 @@ function MyIcon(props) {
   let prefix;
   const {value} = props;
   const address = isU8a(value) || isHex(value) ? encodeAddress(value, prefix) : (value || '');
-  const publicKey = u8aToHex(decodeAddress(address, false, prefix));
+  let publicKey;
+  try {
+    publicKey = u8aToHex(decodeAddress(address, false, prefix));
+  } catch (e) {
+    return null;
+  }
   const {className = '', isAlternative, isHighlight, size = DEFAULT_SIZE, style, theme = 'default'} = props;
   const Component = !address ? Empty : Components['polkadot'];
-  if(!address) {
+  if (!address) {
     return null;
   }
   return (
